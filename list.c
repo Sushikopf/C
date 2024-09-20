10b9d8d9d87191c40f64715d1ea789954cfde651
Sushikopf <sascha_robel@gmx.de>
2024-09-20 11:24:14 +0200

    repaired linked list append function

--- list.c
+++ list.c
@@ -8,22 +8,70 @@ typedef struct node
     struct node *next;
 }node;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     node *list = NULL;
-
-    for(int i = 1; i < argc; i++)
+    //dumb appetion of arguments to list
+    /*for(int i = 1; i < argc; i++)
     {
         int number = atoi(argv[i]);
         node *n = malloc(sizeof(node));
         if(n == NULL)
         {
-            //free memory
+            free(n);
             return 1;
         }
         n->number = number;
         n->next = list;
         list = n;
     }
+    */
+    //smart append to list in sorted order
+    //for each cmd arg 
+    for(int i = 1; i < argc; i++)
+    {
+        //convert arg to int
+        int number = atoi(argv[i]);
+        //created node n of size node
+        node *n = malloc(sizeof(node));
+        //if memory allocation failed terminate
+        if(n == NULL){
+            return 1;
+        }
+        n->number = number;
+        n->next = NULL;
+        printf("n->number = %i\n", n->number);
+        // when list empty, set list to argument
+        if( list == NULL)
+        {
+            list = n;
+        }
+        else if(n->number < list->number)
+        {
+            printf("n-> number: %i is smaller than list-number: %i\n", n->number, list->number);
+            n->next = list;
+            list = n;
+        }else 
+        {
+            for(node *ptr = list; ptr != NULL; ptr = ptr->next)
+            {
+                if(ptr->next == NULL)
+                {
+                    printf("next argument is NULL");
+                    ptr->next = n;
+                    break;
+                }
+                if(n->number < ptr->next->number)
+                {
+                    n->next = ptr->next;
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
+    }
+
+
 // print whole list
     node *ptr = list;
     while(ptr != NULL)
--- main.c
+++ main.c
@@ -1,7 +1,95 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+//note: x->member;
+// is the same as (*x).member; -> meaning call the member variable of variable x of defined type struct
+
+// define structure for list
+typedef struct node_list
+{
+    int number;
+    struct node_list *next;
+
+}node_list;
+
+// define structure for Tree 
+typedef struct node_tree
+{
+    int Number;
+    struct node_tree *left;
+    struct node_tree *right;
+
+}node_tree;
+
+
+bool append(node_list *list, int number);
+bool search_tree(node_tree *tree, int number);
+bool search_list(node_list *list, int number);
 
 int main(void)
 {
-    printf("Hello World!");
+    int i = 10;
+    int* ptr = &i;
+    printf("ptr= %i, i= %i\n",ptr, i);
+    
+    *ptr += 1;
+    printf("ptr= %i, i= %i\n",ptr,i);
+
+    if (true)
+    {
+        printf("true exists\n");
+    }
+    node_list *list = malloc(sizeof(node_list));
+    append(list, 1);
+    printf("%i", list->number);
+
+    free(list);
     return 0;
+}
+// func to search through tree 
+bool search_tree (node_tree *tree, int number)
+{
+
+    if(tree == NULL)
+    {
+        return false;
+    }else if(number< tree->Number){
+        return search_tree(tree->left, number);
+    }else if(number > tree->Number){
+        return search_tree(tree->right, number);
+
+    }else{
+        return true;
+    }
+    
+}
+bool search_list(node_list *list, int number)
+{
+    if(list == NULL)
+    {
+        return false;
+    }else if(number == list->number)
+    {
+        return true;
+    }else
+    {
+        search_list(list->next, number);
+    }
+    return true;
+}
+
+bool append(node_list *list, int number){
+  node_list *n = malloc(sizeof(node_list));
+  if(n == NULL)
+  {
+    return 0;
+  }else
+  {
+    n->number = number;
+    n->next = list;
+    list = n;
+    free(n);
+    return true;
+  }
 }
